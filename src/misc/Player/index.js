1daf591517ad0cf16f38b2f1ac9910d87b9ac31b
LuckyOwlBen <benmalmberg2@gmail.com>
2025-10-15 17:27:17 -0600

    made second logo display on video player auto play

--- src/misc/Player/index.js
+++ src/misc/Player/index.js
@@ -50,75 +50,85 @@ export default function Player({
 			<VideoJS
 				type={type}
 				options={config}
-				onReady={(player) => {
+				onReady={async (player) => {
 					const overlays = [];
 
+					// Function to load image synchronously
+					const loadImage = (src) => {
+						return new Promise((resolve, reject) => {
+							const img = new Image();
+							img.onload = () => resolve(img);
+							img.onerror = reject;
+							img.src = src + '?' + Math.random();
+						});
+					};
+
+					// Load logo if specified
 					if (logo.image.length !== 0) {
-						var overlay = null;
+						try {
+							const imgTag = await loadImage(logo.image);
+							imgTag.setAttribute('width', imgTag.width);
+							imgTag.setAttribute('height', imgTag.height);
 
-						var imgTag = new Image();
-						imgTag.onLoad = function () {
-							imgTag.setAttribute('width', this.width);
-							imgTag.setAttribute('height'.this.height);
-						};
-						imgTag.src = logo.image + '?' + Math.random();
+							let overlay;
+							if (logo.link.length !== 0) {
+								const aTag = document.createElement('a');
+								aTag.setAttribute('href', logo.link);
+								aTag.setAttribute('target', '_blank');
+								aTag.appendChild(imgTag);
+								overlay = aTag.outerHTML;
+							} else {
+								overlay = imgTag.outerHTML;
+							}
 
-						if (logo.link.length !== 0) {
-							var aTag = document.createElement('a');
-							aTag.setAttribute('href', logo.link);
-							aTag.setAttribute('target', '_blank');
-							aTag.appendChild(imgTag);
-							overlay = aTag.outerHTML;
-						} else {
-							overlay = imgTag.outerHTML;
-						}
-
-						overlays.push({
-							showBackground: false,
-							content: overlay,
-							start: 'play',
-							end: 'pause',
-							align: logo.position,
-						});
-					}
-
-					if (logo2.image.length !== 0) {
-						var overlay2 = null;
-
-						var imgTag2 = new Image();
-						imgTag2.onLoad = function () {
-							imgTag2.setAttribute('width', this.width);
-							imgTag2.setAttribute('height'.this.height);
-						};
-						imgTag2.src = logo2.image + '?' + Math.random();
-
-						if (logo2.link.length !== 0) {
-							var aTag2 = document.createElement('a');
-							aTag2.setAttribute('href', logo2.link);
-							aTag2.setAttribute('target', '_blank');
-							aTag2.appendChild(imgTag2);
-							overlay2 = aTag2.outerHTML;
-						} else {
-							overlay2 = imgTag2.outerHTML;
-						}
-
-						overlays.push({
-							showBackground: false,
-							content: overlay2,
-							start: 'play',
-							end: 'pause',
-							align: logo2.position,
-						});
-					}
-
-					// Apply overlays after setup
-					setTimeout(() => {
-						if (overlays.length > 0 && player.overlay) {
-							player.overlay({
-								overlays: overlays,
+							overlays.push({
+								showBackground: false,
+								content: overlay,
+								start: 'play',
+								end: 'pause',
+								align: logo.position,
 							});
+						} catch (e) {
+							// Logo failed to load, continue without it
 						}
-					}, 100);
+					}
+
+					// Load logo2 if specified
+					if (logo2.image.length !== 0) {
+						try {
+							const imgTag2 = await loadImage(logo2.image);
+							imgTag2.setAttribute('width', imgTag2.width);
+							imgTag2.setAttribute('height', imgTag2.height);
+
+							let overlay2;
+							if (logo2.link.length !== 0) {
+								const aTag2 = document.createElement('a');
+								aTag2.setAttribute('href', logo2.link);
+								aTag2.setAttribute('target', '_blank');
+								aTag2.appendChild(imgTag2);
+								overlay2 = aTag2.outerHTML;
+							} else {
+								overlay2 = imgTag2.outerHTML;
+							}
+
+							overlays.push({
+								showBackground: false,
+								content: overlay2,
+								start: 'play',
+								end: 'pause',
+								align: logo2.position,
+							});
+						} catch (e) {
+							// Logo2 failed to load, continue without it
+						}
+					}
+
+					// Apply overlays after all images are loaded
+					if (overlays.length > 0 && player.overlay) {
+						player.overlay({
+							overlays: overlays,
+						});
+					}
 
 					if (autoplay === true) {
 						// https://videojs.com/blog/autoplay-best-practices-with-video-js/
